1b67a2eb34983d7fc0b540f9f58fb0477e44e504
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-11-07 14:57:50 -0300

    Fazendo a api funcionar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api'
 
 function Home(){
+const [filmes, setFilmes] = useState([]);
+
+useEffect(()=>{
+async function loadFilmes() {
+    const response = await api.get("movie/now_playing", {
+        params:{
+            api_key:"6588f59db426156c81ccab94f485a40c",
+            language: "pt-BR",
+            page: 1,
+        }
+    })
+    
+}
+loadFilmes();
+}, [])
+
     return(
         <div>
             <h1>home</h1>
